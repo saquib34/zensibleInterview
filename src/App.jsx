3963145ef68988e43b17fc76adf046aa6479715f
saquib34 <shadmanshahin6@gmail.com>
2024-10-21 04:27:04 +0530

    hhtps

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const IMDBAnalysisDashboard = () => {
   const [animateChart, setAnimateChart] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const Surl = 'http://13.233.91.141:5000';
+  const Surl = 'https://zensible.shadmanshahin6.workers.dev/';
 
 
   useEffect(() => {
