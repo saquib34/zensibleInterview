4daad14384223bb71afc3f24e78bfdbbd87ee005
saquib34 <shadmanshahin6@gmail.com>
2024-10-21 11:26:26 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const IMDBAnalysisDashboard = () => {
   const [animateChart, setAnimateChart] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const Surl = 'https://jtflvprri3.execute-api.ap-south-1.amazonaws.com/prod';
+  const Surl = 'http://13.233.91.141:5000';
 
 
   useEffect(() => {
